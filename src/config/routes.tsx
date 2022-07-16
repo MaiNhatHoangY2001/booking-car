f34aa24c350f62309ccf1fad57c53a51e2915042
hoang <hoan44pro@gmail.com>
2022-07-16 10:19:02 +0700

    fix link

--- src/config/routes.tsx
+++ src/config/routes.tsx
@@ -1,14 +1,14 @@
 const routes = {
-    dashboard: "/",
-    assets: "/assets",
-    booking: "/booking",
-    sell_cars: "/sell_cars",
-    buy_cars: "/buy_cars",
-    services: "/services",
-    calender: "/calender",
-    messages: "/messages",
-    setting: "/setting",
-    logout: "/logout",
+    dashboard: "/booking-car",
+    assets: "/booking-car/assets",
+    booking: "/booking-car/booking",
+    sell_cars: "/booking-car/sell_cars",
+    buy_cars: "/booking-car/buy_cars",
+    services: "/booking-car/services",
+    calender: "/booking-car/calender",
+    messages: "/booking-car/messages",
+    setting: "/booking-car/setting",
+    logout: "/booking-car/logout",
 }
 
 export default routes;
